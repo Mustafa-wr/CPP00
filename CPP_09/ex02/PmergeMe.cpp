4215d945b88470fe415abd6f199e46618da0e792
must <mradwan@student.42abudhabi.ae>
2023-07-05 21:56:17 +0400

    '

--- CPP_09/ex02/PmergeMe.cpp
+++ CPP_09/ex02/PmergeMe.cpp
@@ -6,23 +6,97 @@
 /*   By: mradwan <mradwan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/03 19:41:04 by mradwan           #+#    #+#             */
-/*   Updated: 2023/07/05 16:15:45 by mradwan          ###   ########.fr       */
+/*   Updated: 2023/07/05 20:48:51 by mradwan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
 
-// PmergeMe::PmergeMe() {}
+void	sorting1(std::vector<int> &bar)
+{
+	if(bar.size() < 2)
+		return ;
+	
+	std::vector<int> left;
+	std::vector<int> right;
+	
+	size_t mid = bar.size() / 2;
 
-// PmergeMe::PmergeMe(const PmergeMe &src)
-// {
-// 	*this = src;
-// }
+	for (size_t i = 0; i < mid; i++)
+		left.push_back(bar[i]);
+	for (size_t i = 0; i < (bar.size()) - mid; i++)
+		right.push_back(bar[mid + i]);
 
-// PmergeMe	&PmergeMe::operator=(const PmergeMe &Or)
-// {
-// 	return (*this);
-// }
+	sorting1(left);
+	sorting1(right);
+	
+	size_t i = 0, j = 0, c = 0;
+	while (j < left.size() && c < right.size())
+	{
+		if(left[j] < right[c])
+			bar[i] = left[j++];
+		else
+			bar[i] = right[c++];
+		i++;
+	}
+	while (j < left.size())
+		bar[i++] = left[j++];
+	while (c < right.size())
+		bar[i++] = right[c++];
 
-// PmergeMe::~PmergeMe() {}
+}
 
+
+void	sorting2(std::list<int> &bar)
+{
+	if(bar.size() < 2)
+		return ;
+	
+	std::list<int> left;
+	std::list<int> right;
+	std::list<int>::iterator it = bar.begin();
+
+	size_t mid = bar.size() / 2;
+
+	for (size_t i = 0; i < mid; i++)
+	{
+		left.push_back(*it);
+		it++;
+	}
+	for (size_t i = mid; i < bar.size(); i++)
+	{
+		right.push_back(*it);
+		it++;
+	}
+
+	sorting2(left);
+	sorting2(right);
+	
+	bar.clear();
+
+    std::list<int>::iterator leftIt = left.begin();
+    std::list<int>::iterator rightIt = right.begin();
+    while (leftIt != left.end() && rightIt != right.end())
+	{
+		if (*leftIt <= *rightIt)
+		{
+			bar.push_back(*leftIt);
+			leftIt++;
+		}
+		else
+		{
+			bar.push_back(*rightIt);
+			rightIt++;
+		}
+    }
+    while (leftIt != left.end())
+	{
+      bar.push_back(*leftIt);
+      leftIt++;
+    }
+    while (rightIt != right.end())
+	{
+      bar.push_back(*rightIt);
+      rightIt++;
+    }
+}
--- CPP_09/ex02/main.cpp
+++ CPP_09/ex02/main.cpp
@@ -6,101 +6,13 @@
 /*   By: mradwan <mradwan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/03 21:13:25 by mradwan           #+#    #+#             */
-/*   Updated: 2023/07/05 20:06:54 by mradwan          ###   ########.fr       */
+/*   Updated: 2023/07/05 21:26:18 by mradwan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
 
-void	sorting1(std::vector<int> &bar)
-{
-	if(bar.size() < 2)
-		return ;
-	
-	std::vector<int> left;
-	std::vector<int> right;
-	
-	size_t mid = bar.size() / 2;
-
-	for (size_t i = 0; i < mid; i++)
-		left.push_back(bar[i]);
-	for (size_t i = 0; i < (bar.size()) - mid; i++)
-		right.push_back(bar[mid + i]);
-
-	sorting1(left);
-	sorting1(right);
-	
-	size_t i = 0, j = 0, c = 0;
-	while (j < left.size() && c < right.size())
-	{
-		if(left[j] < right[c])
-			bar[i] = left[j++];
-		else
-			bar[i] = right[c++];
-		i++;
-	}
-	while (j < left.size())
-		bar[i++] = left[j++];
-	while (c < right.size())
-		bar[i++] = right[c++];
-
-}
-
-void	sorting2(std::list<int> &bar)
-{
-	if(bar.size() < 2)
-		return ;
-	
-	std::list<int> left;
-	std::list<int> right;
-	std::list<int>::iterator it = bar.begin();
-
-	size_t mid = bar.size() / 2;
-
-	for (size_t i = 0; i < mid; i++)
-	{
-		left.push_back(*it);
-		it++;
-	}
-	for (size_t i = mid; i < bar.size(); i++)
-	{
-		right.push_back(*it);
-		it++;
-	}
-
-	sorting2(left);
-	sorting2(right);
-	
-	bar.clear();
-
-    std::list<int>::iterator leftIt = left.begin();
-    std::list<int>::iterator rightIt = right.begin();
-    while (leftIt != left.end() && rightIt != right.end())
-	{
-		if (*leftIt <= *rightIt)
-		{
-			bar.push_back(*leftIt);
-			leftIt++;
-		}
-		else
-		{
-			bar.push_back(*rightIt);
-			rightIt++;
-		}
-    }
-    while (leftIt != left.end())
-	{
-      bar.push_back(*leftIt);
-      leftIt++;
-    }
-    while (rightIt != right.end())
-	{
-      bar.push_back(*rightIt);
-      rightIt++;
-    }
-}
-
-int parse(char **av)
+static int parse(char **av)
 {
 	int i = 1, j = 0;
 	while (av[i])
@@ -192,5 +104,6 @@ int main(int ac, char **av)
 
 	std::cout << "time to proscess " << list.size() << " elements with std::list : " \
 			  << static_cast<float>(end2 - start2) / 10000 << std::endl;
+	
 	return 0;
 }
